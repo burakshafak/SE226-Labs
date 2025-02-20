837f564d65bbf2b7d60010d42d399ad9b45866bc
Burak Şafak <85990336+burakshafak@users.noreply.github.com>
2025-02-20 03:24:48 -0700

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,33 @@
+x = input("What is your name")
+print(x)
+print("Hello " , x)
+y = input("What is your ID?: ")
+print(y)
+print("Your ID is " , y)
+
+var1 = int(input("enter first value:"))
+var2 = int(input("enter second value:"))
+sum = var1 + var2
+diff = abs(var1 - var2)
+prod = var1 * var2
+print("Sum is: " , sum)
+print("Diff is: ", abs(diff))
+print("Prod is" , prod)
+
+print(" ")
+name = input("What is your name")
+labgrade = int(input("lab grade"))
+midtermgrade = int(input("midterm grade"))
+finalgrade = int(input("final grade"))
+lastScore = labgrade/4 + midtermgrade*(35/100) + finalgrade*(4/10)
+
+print(" ")
+print("Name: ", name)
+print("Lab: ", labgrade)
+print("Midterm: ", midtermgrade)
+print("Final: ", finalgrade)
+print("Last Score: ", lastScore)
+
+print(" ")
+
+print(" *\n","**\n","***\n", "**\n", "*")
